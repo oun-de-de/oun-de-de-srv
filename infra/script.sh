d15f7670d41511cea9ae2f364a7ec5bedbc0f293
I753911 <daniel.nguyen03@sap.com>
2026-01-16 16:26:09 +0700

    feat: implement nginx as container

---
+++ infra/script.sh
@@ -0,0 +1,69 @@
+cat <<EOF > nginx.conf
+worker_processes auto;
+
+events {
+    worker_connections 2048;
+}
+
+http {
+    upstream http_backend {
+       server srv:8080;
+    }
+
+    server {
+        listen 80;
+        listen [::]:80;
+
+        server_name oun-de-de-srv.cloud;
+
+        location / {
+            proxy_pass http://http_backend;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+        }
+    }
+}
+EOF
+
+cat <<EOF > docker-compose.yaml
+services:
+  db:
+    image: mysql:8.4.7
+    environment:
+      MYSQL_ROOT_PASSWORD: rootpassword
+      MYSQL_DATABASE: UserDB
+      MYSQL_USER: admin
+      MYSQL_PASSWORD: Admin123
+    restart: unless-stopped
+    networks:
+      - app-network
+
+  srv:
+    image: dacnguyen9101/oun-de-de:$IMAGE_TAG
+    depends_on:
+      - db
+    restart: unless-stopped
+    networks:
+      - app-network
+
+  nginx:
+    image: nginx:1.24.0
+    ports:
+      - "80:80"
+    depends_on:
+      - srv
+    volumes:
+      - ./nginx.conf:/etc/nginx/nginx.conf:ro
+    networks:
+      - app-network
+
+networks:
+  app-network:
+    driver: bridge
+EOF
+
+docker compose down
+docker compose pull
+docker compose up -d
