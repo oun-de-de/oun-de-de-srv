8503ab07a9415e89424ec6574392883adc02c14b
I753911 <daniel.nguyen03@sap.com>
2026-01-19 01:09:13 +0700

    feat: verify login user

--- src/main/java/com/cdtphuhoi/oun_de_de/controllers/AuthController.java
+++ src/main/java/com/cdtphuhoi/oun_de_de/controllers/AuthController.java
@@ -5,6 +5,8 @@ import com.cdtphuhoi.oun_de_de.services.auth.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,15 +24,15 @@ public class AuthController {
 
     @PostMapping("/token")
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-//        var authentication = authenticationManager.authenticate(
-//            new UsernamePasswordAuthenticationToken(
-//                authRequest.getUsername(),
-//                authRequest.getPassword()
-//            )
-//        );
-//        if (!authentication.isAuthenticated()) {
-//            throw new UsernameNotFoundException("Invalid user request!");
-//        }
+        var authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                authRequest.getUsername(),
+                authRequest.getPassword()
+            )
+        );
+        if (!authentication.isAuthenticated()) {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
         return jwtService.generateToken(authRequest.getUsername());
     }
 }
