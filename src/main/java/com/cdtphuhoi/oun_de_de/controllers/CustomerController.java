f54b0c8fd2066cb9088cb2ba0bfa0817d5872937
I753911 <daniel.nguyen03@sap.com>
2026-01-24 13:21:46 +0700

    chores: disable jacoco

--- src/main/java/com/cdtphuhoi/oun_de_de/controllers/CustomerController.java
+++ src/main/java/com/cdtphuhoi/oun_de_de/controllers/CustomerController.java
@@ -1,5 +1,7 @@
 package com.cdtphuhoi.oun_de_de.controllers;
 
+import static com.cdtphuhoi.oun_de_de.common.Constants.SWAGGER_SECURITY_SCHEME_NAME;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
+@SecurityRequirement(name = SWAGGER_SECURITY_SCHEME_NAME)
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
 //    private final UserRepository userRepository;
 //
