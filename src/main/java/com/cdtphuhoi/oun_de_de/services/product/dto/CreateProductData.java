53a5a92853a2a787154d8731212fd31b4a3d9110
I753911 <daniel.nguyen03@sap.com>
2026-02-06 13:49:11 +0700

    fix: missing mapiong product data

--- src/main/java/com/cdtphuhoi/oun_de_de/services/product/dto/CreateProductData.java
+++ src/main/java/com/cdtphuhoi/oun_de_de/services/product/dto/CreateProductData.java
@@ -2,8 +2,21 @@ package com.cdtphuhoi.oun_de_de.services.product.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @Builder
 public class CreateProductData {
+    private String name;
+
+    private Date date;
+
+    private String refNo;
+
+    private BigDecimal quantity;
+
+    private BigDecimal cost;
+
+    private BigDecimal price;
 }
