e13c74ca7ab61628141de0904ff469f5efcb4149
I753911 <daniel.nguyen03@sap.com>
2026-02-08 00:21:47 +0700

    feat: add CodeListRequest

---
+++ src/main/java/com/cdtphuhoi/oun_de_de/common/CodeListRequest.java
@@ -0,0 +1,20 @@
+package com.cdtphuhoi.oun_de_de.common;
+
+import static com.cdtphuhoi.oun_de_de.common.Constants.DEFAULT_DESCRIPTION_FIELD_LENGTH;
+import static com.cdtphuhoi.oun_de_de.common.Constants.DEFAULT_STRING_FIELD_LENGTH;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Getter
+@Setter
+public class CodeListRequest {
+
+    @NotBlank
+    @Size(max = DEFAULT_STRING_FIELD_LENGTH)
+    private String name;
+
+    @Size(max = DEFAULT_DESCRIPTION_FIELD_LENGTH)
+    private String descr;
+}
--- src/main/java/com/cdtphuhoi/oun_de_de/controllers/dto/settings/CreateWarehouseRequest.java
+++ src/main/java/com/cdtphuhoi/oun_de_de/controllers/dto/settings/CreateWarehouseRequest.java
@@ -1,15 +1,7 @@
 package com.cdtphuhoi.oun_de_de.controllers.dto.settings;
 
-import static com.cdtphuhoi.oun_de_de.common.Constants.DEFAULT_DESCRIPTION_FIELD_LENGTH;
-import static com.cdtphuhoi.oun_de_de.common.Constants.DEFAULT_STRING_FIELD_LENGTH;
-import lombok.Data;
-import jakarta.validation.constraints.Size;
+import com.cdtphuhoi.oun_de_de.common.CodeListRequest;
 
-@Data
-public class CreateWarehouseRequest {
-    @Size(max = DEFAULT_STRING_FIELD_LENGTH)
-    private String name;
+public class CreateWarehouseRequest extends CodeListRequest {
 
-    @Size(max = DEFAULT_DESCRIPTION_FIELD_LENGTH)
-    private String descr;
 }
