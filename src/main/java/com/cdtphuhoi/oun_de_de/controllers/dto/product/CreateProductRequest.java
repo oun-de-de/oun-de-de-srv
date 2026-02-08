f58ec5adca93c49e898d7d85d660b43716fca943
I753911 <daniel.nguyen03@sap.com>
2026-02-08 21:55:33 +0700

    chores: checkstyle

--- src/main/java/com/cdtphuhoi/oun_de_de/controllers/dto/product/CreateProductRequest.java
+++ src/main/java/com/cdtphuhoi/oun_de_de/controllers/dto/product/CreateProductRequest.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import java.util.UUID;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @Data
 public class CreateProductRequest {
