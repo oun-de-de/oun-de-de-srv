33ae4c76fd80a76cc1142aa1d1ff525e561e7670
I753911 <daniel.nguyen03@sap.com>
2026-02-22 15:38:11 +0700

    chore: rename

--- src/main/java/com/cdtphuhoi/oun_de_de/jobs/OunDeDeApplicationScheduledJob.java
+++ src/main/java/com/cdtphuhoi/oun_de_de/jobs/OunDeDeApplicationScheduledJob.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ScheduledFixedRateExample {
+public class OunDeDeApplicationScheduledJob {
 
     private final LoanInstallmentRepository loanInstallmentRepository;
 
