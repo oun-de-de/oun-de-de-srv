7c4c6f9992ac7a75d8c8f55a0d86121458cebd01
I753911 <daniel.nguyen03@sap.com>
2026-03-12 02:03:25 +0700

    feat: seeding customer data

---
+++ src/main/java/com/cdtphuhoi/oun_de_de/controllers/SeedController.java
@@ -0,0 +1,4 @@
+package com.cdtphuhoi.oun_de_de.controllers;
+
+public class SeedController {
+}
---
+++ src/main/java/com/cdtphuhoi/oun_de_de/utils/ExcelUtils.java
@@ -0,0 +1,31 @@
+package com.sap.sbnc.manufacturing.collaboration.commons.utils;
+
+import lombok.experimental.UtilityClass;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+@UtilityClass
+public class ExcelUtils {
+
+    public static Row getOrCreateRow(Sheet sheet, int rowIndex) {
+        var row = sheet.getRow(rowIndex);
+        if (row == null) {
+            row = sheet.createRow(rowIndex);
+        }
+        return row;
+    }
+
+    public static String getCellStringValue(Cell cell) {
+        if (cell == null) {
+            return null;
+        }
+
+        return switch (cell.getCellType()) {
+            case NUMERIC -> String.valueOf((long) cell.getNumericCellValue());
+            case BOOLEAN -> String.valueOf(cell.getBooleanCellValue());
+            case FORMULA -> cell.getCellFormula();
+            default -> cell.getStringCellValue().trim();
+        };
+    }
+}
