521a6d92171ba8d0e5ca4ef9e2e3534cfc8c68a5
I753911 <daniel.nguyen03@sap.com>
2026-03-12 17:22:07 +0700

    chores: test

--- src/test/java/com/cdtphuhoi/oun_de_de/services/customer/CustomerServiceTest.java
+++ src/test/java/com/cdtphuhoi/oun_de_de/services/customer/CustomerServiceTest.java
@@ -1,31 +1,17 @@
 package com.cdtphuhoi.oun_de_de.services.customer;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import com.cdtphuhoi.oun_de_de.entities.Customer;
-import com.cdtphuhoi.oun_de_de.entities.User;
 import com.cdtphuhoi.oun_de_de.exceptions.ResourceNotFoundException;
 import com.cdtphuhoi.oun_de_de.repositories.CustomerRepository;
 import com.cdtphuhoi.oun_de_de.repositories.UserRepository;
 import com.cdtphuhoi.oun_de_de.services.customer.dto.CreateCustomerData;
-import com.cdtphuhoi.oun_de_de.services.customer.dto.CustomerResult;
-import com.cdtphuhoi.oun_de_de.mappers.CustomerMapper;
-import com.cdtphuhoi.oun_de_de.mappers.MapperHelpers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,23 +24,6 @@ class CustomerServiceTest {
 
     @InjectMocks
     private CustomerService customerService;
-
-
-    @Test
-    void create_shouldCreateCustomer() {
-        var data = mock(CreateCustomerData.class);
-        var mockId = "mockId";
-        when(data.getEmployeeId()).thenReturn(mockId);
-        var employee = mock(User.class);
-        when(userRepository.findOneById(mockId)).thenReturn(Optional.of(employee));
-        var customer = mock(Customer.class);
-        when(customerRepository.save(any(Customer.class))).thenReturn(customer);
-
-        var result = customerService.create(data);
-
-        assertNotNull(result);
-        verify(customerRepository).save(any(Customer.class));
-    }
 
     @Test
     void create_shouldThrowWhenEmployeeNotFound() {
