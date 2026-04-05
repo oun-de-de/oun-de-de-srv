7bd6c1ba786d379fc1b59d250a272d8603a3bed6
I753911 <daniel.nguyen03@sap.com>
2026-04-05 14:32:33 +0700

    chores: checkstyle

--- src/main/java/com/cdtphuhoi/oun_de_de/entities/InventoryItem.java
+++ src/main/java/com/cdtphuhoi/oun_de_de/entities/InventoryItem.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import java.math.BigDecimal;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -21,8 +20,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
