b224e44b7aba44f880a97f54cd073202474f2f85
I753911 <daniel.nguyen03@sap.com>
2026-04-28 03:15:13 +0700

    feat: update auto script

--- infra/script.sh
+++ infra/script.sh
@@ -1,3 +1,19 @@
+cat <<EOF > .env
+APP_DOMAIN=$APP_DOMAIN
+SSL_EMAIL=$SSL_EMAIL
+EOF
+
+cat <<EOF > 99-autoreload.sh
+# 99-autoreload.sh
+
+#!/bin/sh
+while :; do
+    # Optional: Instead of sleep, detect config changes and only reload if necessary.
+    sleep 6h
+    nginx -t && nginx -s reload
+done &
+EOF
+
 cat <<EOF > nginx.conf
 worker_processes auto;
 
@@ -14,23 +30,33 @@ http {
         listen 80;
         listen [::]:80;
 
-        server_name oun-de-de-srv.cloud;
+        server_name $APP_DOMAIN;
 
-        return 301 https://\$host\$request_uri;
+
+        location / {
+            return 301 https://\$host\$request_uri;
+        }
+
+        location /.well-known/acme-challenge/ {
+            root /var/www/certbot;
+        }
     }
 
     server {
         listen 443 ssl;
         listen [::]:443 ssl;
 
-        server_name oun-de-de-srv.cloud;
+        server_name $APP_DOMAIN;
 
-        ssl_certificate     /etc/letsencrypt/live/oun-de-de-srv.cloud/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/oun-de-de-srv.cloud/privkey.pem;
+        ssl_certificate     /etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/$APP_DOMAIN/privkey.pem;
+        include /etc/letsencrypt/options-ssl-nginx.conf;
+        ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 
         location / {
             proxy_pass http://http_backend;
 
+            proxy_set_header X-Url-Scheme \$scheme;
             proxy_set_header Host \$host;
             proxy_set_header X-Real-IP \$remote_addr;
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -57,6 +83,7 @@ services:
 
   srv:
     image: dacnguyen9101/oun-de-de:$IMAGE_TAG
+    # restart: unless-stopped
     # depends_on:
     #   - db
     environment:
@@ -69,23 +96,34 @@ services:
       CORS_CONFIG_PROPERTIES_ALLOWED_ORIGINS: "*"
     # CORS_CONFIG_PROPERTIES_ALLOWED_ORIGINS: "http://cdtphuhoi.com,https://cdtphuhoi.com"
     # SPRING_JPA_HIBERNATE_DDL_AUTO: create-drop
-    # restart: unless-stopped
     networks:
       - app-network
 
   nginx:
     image: nginx:1.24.0
+    restart: unless-stopped
     ports:
       - "80:80"
       - "443:443"
     depends_on:
       - srv
+    environment:
+      APP_DOMAIN: $APP_DOMAIN
+      SSL_EMAIL: $SSL_EMAIL
     volumes:
       - ./nginx.conf:/etc/nginx/nginx.conf:ro
-      - /etc/letsencrypt/live/oun-de-de-srv.cloud/fullchain.pem:/etc/letsencrypt/live/oun-de-de-srv.cloud/fullchain.pem:ro
-      - /etc/letsencrypt/live/oun-de-de-srv.cloud/privkey.pem:/etc/letsencrypt/live/oun-de-de-srv.cloud/privkey.pem:ro
+      - ./nginx/certbot/conf:/etc/letsencrypt
+      - ./nginx/certbot/www:/var/www/certbot
+      - ./99-autoreload.sh:/docker-entrypoint.d/99-autoreload.sh
     networks:
       - app-network
+
+  certbot:
+    image: certbot/certbot:latest
+    volumes:
+      - ./nginx/certbot/conf:/etc/letsencrypt
+      - ./nginx/certbot/www:/var/www/certbot
+    entrypoint: "/bin/sh -c 'trap exit TERM; while :; do certbot renew; sleep 12h & wait \$\${!}; done;'"
 
 networks:
   app-network:
